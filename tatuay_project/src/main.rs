e0084821bf76356e74b1fa693931f8e891ec4714
oneduck11034 <oneduck11034@gmail.com>
2024-10-25 18:13:32 +0000

    concept

--- tatuay_project/src/main.rs
+++ tatuay_project/src/main.rs
@@ -2,9 +2,13 @@ fn main() {
     if cfg!(target_os = "windows") {
         println!("Running on Windows!");
         // Add Windows-specific code here
+        
+        // https://github.com/zxcV32/unlimited-folders-on-windows-desktop/blob/master/virus.bat
     } else if cfg!(target_os = "linux") {
         println!("Running on Linux!");
         // Add Linux-specific code here
+        
+        // https://stackoverflow.com/questions/40613898/how-to-force-immediate-writes-to-a-disk-from-our-own-driver-on-linux
     } else {
         println!("Running on an unsupported OS!");
     }
